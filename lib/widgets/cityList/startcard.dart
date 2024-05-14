278fa8e027d35968e96a051420d298c95780dbef
DubstepTC <dubsteptc@mail.ru>
2024-05-14 09:06:15 +0300

    app

--- lib/widgets/cityList/startcard.dart
+++ lib/widgets/cityList/startcard.dart
@@ -1,8 +1,13 @@
+import 'package:SkyView/API/futureApi.dart';
 import 'package:SkyView/API/openApi.dart';
+import 'package:SkyView/API/timeZoneName.dart';
+import 'package:SkyView/API/updateApi.dart';
 import 'package:SkyView/Appconstants/constants.dart';
 import 'package:SkyView/pages/main_page.dart';
+import 'package:SkyView/pages/start/entry.dart';
 import 'package:flutter/material.dart';
 import 'package:SkyView/function/weatherImage.dart'; 
+import 'package:intl/intl.dart';  
 
 class StartCardCities extends StatefulWidget {
   final double width;
@@ -21,30 +26,88 @@ class StartCardCities extends StatefulWidget {
   class _StartCardCitiesState extends State<StartCardCities> {
 
   Future add(cityData) async {
-  bool cityExists = AppConstants.cityCountryMap.any((element) =>
-      element['city'].toLowerCase() == cityData['city'].toLowerCase());
+    bool cityExists = AppConstants.cityCountryMap.any((element) =>
+        element['city'].toLowerCase() == cityData['city'].toLowerCase());
 
-  if (!cityExists) {
-    AppConstants.cityCountryMap.add(cityData);
-  } else {
-    return const AlertDialog(
-      backgroundColor: Color.fromRGBO(39, 64, 87, 1),
-      content:  Text(
-        'Город уже выбран для отображения', 
-        textAlign: TextAlign.center, 
-        style: TextStyle(
-          color: Color.fromRGBO(194, 184, 255, 1), 
-          fontSize: 24, 
-          fontWeight: FontWeight.bold
+    if (!cityExists) {
+      AppConstants.cityCountryMap.add(cityData);
+    } else {
+      return const AlertDialog(
+        backgroundColor: Color.fromRGBO(39, 64, 87, 1),
+        content:  Text(
+          'Город уже выбран для отображения', 
+          textAlign: TextAlign.center, 
+          style: TextStyle(
+            color: Color.fromRGBO(194, 184, 255, 1), 
+            fontSize: 24, 
+            fontWeight: FontWeight.bold
+          ),
         ),
-      ),
-    );
+      );
+    }
+  } 
+
+  Future<void> _loadData() async {
+    FutureApi future = FutureApi();
+    Updateapi weather = Updateapi();
+    timeZoneName timezone = timeZoneName();
+    List<Future> futures = [];
+    List<Map<String, dynamic>> days = forecastForFiveDays();
+
+    for (var cityMap in AppConstants.cityCountryMap) {
+      String city = cityMap["city"];
+      var weatherMain = await weather.getWeather(city);
+      if (weatherMain != null) {
+          futures.add(weatherMain);
+      }
+    }
+
+    for (var day in days) {
+      String city = day["city"];
+      String date = day["date"];
+      var weather = await future.getWeather(city, date);
+      if (weather != null) {
+          futures.add(weather);
+      }
+    }
+
+    for (var cityMap in AppConstants.cityCountryMap) {
+      String city = cityMap["city"];
+      var time = await timezone.getWeather(city);
+      if (time != null) {
+          futures.add(time);
+      }
+    }
+
+    await Future.wait(futures);
+
+    for(int i = 0; i < AppConstants.cityCountryMap.length; i++){
+      if (AppConstants.cityCountryMap[i]["city"] == AppConstants.weather[i]["city"]){
+        AppConstants.cityCountryMap[i]["temperature"] = AppConstants.weather[i]["temperature"];
+        AppConstants.cityCountryMap[i]["weather_status"] = AppConstants.weather[i]["weather_status"];
+      }
+    }
   }
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => MainScreen(currentIndex: 0,)),
-  );
-} 
+
+  List<Map<String, dynamic>> forecastForFiveDays() {
+    List<Map<String, dynamic>> days = [];
+    DateTime today = DateTime.now();
+
+    for (var cityMap in AppConstants.cityCountryMap) {
+      String city = cityMap["city"];
+      for (int i = 0; i <= 4; i++) {
+        DateTime nextDate = today.add(Duration(days: i));
+        DateFormat formatter = DateFormat('yyyy-MM-dd');
+        String formattedDate = formatter.format(nextDate);
+        Map<String, dynamic> future = {
+          "city": city,
+          "date": formattedDate
+        };
+        days.add(future);
+      }
+    }
+    return days;
+  }
   WeatherScreen wap = WeatherScreen();
   
   @override
@@ -100,75 +163,6 @@ class StartCardCities extends StatefulWidget {
           ),
         ),
         GestureDetector(
-          onTap: () async{
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  backgroundColor: Color.fromRGBO(39, 64, 87, 1),
-                  content: const Text(
-                    'Хотите отслеживать погоду в городе?', 
-                    textAlign: TextAlign.center, 
-                    style: TextStyle(
-                      color: Color.fromRGBO(194, 184, 255, 1), 
-                      fontSize: 24, 
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  actions: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(false);
-                          },
-                          child: const Text(
-                            'Нет', 
-                            textAlign: TextAlign.center, 
-                            style: TextStyle(
-                              color: Color.fromRGBO(194, 184, 255, 1), 
-                              fontSize: 24, 
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 20,),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(true);
-                          },
-                          child: const Text(
-                            'Да', 
-                            textAlign: TextAlign.center, 
-                            style: TextStyle(
-                              color: Color.fromRGBO(194, 184, 255, 1), 
-                              fontSize: 24, 
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              },
-            ).then((value) async {
-              if (value != null && value) {
-                AppConstants.cityWeather = [];
-                await wap.getWeather(widget.city);
-                Map<String, dynamic> cityData = {
-                  "city": AppConstants.cityWeather[0]["city"],
-                  "country": AppConstants.cityWeather[0]["country"],
-                  "temperature": AppConstants.cityWeather[0]["temperature"],
-                  "weather_status": AppConstants.cityWeather[0]["weather_status"],
-                };
-                AppConstants.cityWeather = [];
-                add(cityData);
-                AppConstants.savePreferences();
-              }
-            });
-          },
           child: Container(
             width: rectangleWidth,
             height: rectangleHeight,
